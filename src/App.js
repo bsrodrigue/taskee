4b8e67ee4743dfb3d62b4d95ed6b919fbc486697
bsrodrigue <bsrodrigue@gmail.com>
2020-09-30 12:28:23 +0000

    Make it simple

--- src/App.js
+++ src/App.js
@@ -9,20 +9,6 @@ import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
 
 import './App.css';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCzIvicS1aZNOM0ItpCCP63qpa_9hmqroo",
-  authDomain: "portfolio-de182.firebaseapp.com",
-  databaseURL: "https://portfolio-de182.firebaseio.com",
-  projectId: "portfolio-de182",
-  storageBucket: "portfolio-de182.appspot.com",
-  messagingSenderId: "318388871042",
-  appId: "1:318388871042:web:378f2912eb11a6a8b5f99b"
-};
-const firebase = require("firebase");
-require("firebase/firestore");
-firebase.initializeApp(firebaseConfig);
-var db = firebase.firestore();
-
 function Header({ title }) {
   return (
     <div className="Header">
@@ -31,22 +17,27 @@ function Header({ title }) {
   );
 }
 
-function TaskCollectionItem({ setCollections, collections, i, title, setCurrentCollection }) {
+function TaskCollectionItem({ currentCollection, setCollections, collections, i, title, setCurrentCollection }) {
   const [edit, setEdit] = useState(false);
   const [newTitle, setNewTitle] = useState('');
 
   const handleClick = () => {
-    console.log(i);
+    
     setCurrentCollection(i);
   }
 
   const handleEdit = () => {
-    console.log("Edit");
+    
     setEdit(true);
   }
 
+  const handleDelete = () => {
+    collections.splice(currentCollection, 1);
+    setCollections([...collections]);
+  }
+
   const handleEditConfirm = () => {
-    console.log("Confirm edit");
+    
     let collection = collections[i];
     collection.title = newTitle;
     collections[i] = collection;
@@ -58,7 +49,8 @@ function TaskCollectionItem({ setCollections, collections, i, title, setCurrentC
     return (
       <>
         <div className="TaskCollectionButton">
-          <TextField value={newTitle} id="standard-basic" label="Enter new name" onChange={(event) => setNewTitle(event.target.value)} />
+          <TextField value={newTitle} id="standard-basic" label="Enter new name"
+          onChange={(event) => setNewTitle(event.target.value)} />
           <div>
 
             <IconButton onClick={handleEditConfirm}>
@@ -82,7 +74,7 @@ function TaskCollectionItem({ setCollections, collections, i, title, setCurrentC
             <IconButton onClick={handleEdit}>
               <EditIcon />
             </IconButton>
-            <IconButton color="secondary">
+            <IconButton color="secondary" onClick={handleDelete}>
               <DeleteIcon />
             </IconButton>
           </div>
@@ -95,7 +87,7 @@ function TaskCollectionItem({ setCollections, collections, i, title, setCurrentC
 
 }
 
-function TaskCollections({ setCollections, collections, setCurrentCollection }) {
+function TaskCollections({ setCollections, collections, setCurrentCollection, currentCollection }) {
 
   const handleCollectionCreation = () => {
     let newCollection = {
@@ -112,12 +104,17 @@ function TaskCollections({ setCollections, collections, setCurrentCollection }) 
       <div className="TaskCollections">
         {
           collections.map((collection, index) => (
-            <TaskCollectionItem setCollections={setCollections} collections={collections} setCurrentCollection={setCurrentCollection} i={index} key={index} title={collection.title} />
+            <TaskCollectionItem currentCollection={currentCollection}
+            setCollections={setCollections} collections={collections}
+            setCurrentCollection={setCurrentCollection}
+            i={index} key={index} title={collection.title} />
           ))
         }
-        <Button className="TaskCollectionCreationButton" variant="contained" color="primary" disableElevation onClick={handleCollectionCreation}>
+        <Button className="TaskCollectionCreationButton" 
+          variant="contained" color="primary" disableElevation 
+          onClick={handleCollectionCreation}>
           Create Task Collection
-</Button>
+        </Button>
       </div>
     </>
   );
@@ -138,15 +135,36 @@ function TaskCreationForm({ collections, setCollections, currentCollection }) {
 
   return (
     <div className="TaskCreationForm">
-      <TextField value={title} onChange={(event) => setTitle(event.target.value)} className="TaskCreationInput" id="filled-basic" label="Create a new Task" variant="filled" />
-      <Button className="TaskCreationButton" variant="contained" color="primary" disableElevation onClick={handleClick}>
+      <TextField value={title}
+        onChange={(event) => setTitle(event.target.value)}
+        className="TaskCreationInput" id="filled-basic"
+        label="Create a new Task" variant="filled" />
+      <Button className="TaskCreationButton"
+        variant="contained" color="primary"
+        disableElevation onClick={handleClick}>
         Create Task
-</Button>
+      </Button>
     </div>
   );
 }
 
 function Task({ i, task, collections, setCollections, currentCollection }) {
+  const [edit, setEdit] = useState(false);
+  const [checked, setChecked] = useState(task.checked);
+  const [newTitle, setNewTitle] = useState('');
+
+
+  const handleEdit = () => {
+    setEdit(true);
+  }
+  const handleEditConfirm = () => {
+    let task = collections[currentCollection].collection[i];
+    task.title = newTitle;
+    collections[currentCollection][i] = task;
+    setCollections([...collections]);
+    setEdit(false);
+  }
+
 
   const handleDelete = () => {
     let collection = collections[currentCollection];
@@ -155,23 +173,42 @@ function Task({ i, task, collections, setCollections, currentCollection }) {
     setCollections([...collections]);
   }
 
-  return (
-    <>
-      <div className="TaskContainer">
-        <p>{task.title}</p>
-        <div className="TaskActions">
-          <Checkbox checked={task.checked} />
-          <IconButton>
-            <EditIcon />
-          </IconButton>
-          <IconButton color="secondary" onClick={handleDelete}>
-            <DeleteIcon />
-          </IconButton>
+  if (edit) {
+    return (
+      <>
+        <div className="TaskContainer">
+          <TextField value={newTitle} id="standard-basic" 
+          label="Enter new name" onChange={(event) => setNewTitle(event.target.value)} />
+        <div>
+
+            <IconButton onClick={handleEditConfirm}>
+              <CheckCircleOutlineIcon />
+            </IconButton>
+
+          </div>
         </div>
-      </div>
-      <hr />
-    </>
-  );
+        <hr />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className="TaskContainer">
+          <p>{task.title}</p>
+          <div className="TaskActions">
+            <Checkbox checked={checked} onChange={event=>setChecked(!checked)}/>
+            <IconButton onClick={handleEdit}>
+              <EditIcon />
+            </IconButton>
+            <IconButton color="secondary" onClick={handleDelete}>
+              <DeleteIcon />
+            </IconButton>
+          </div>
+        </div>
+        <hr />
+      </>
+    );
+  }
 }
 
 function TaskList({ collection, collections, setCollections, currentCollection }) {
@@ -201,10 +238,16 @@ function TaskCollection({ collections, collection, setCollections, currentCollec
 function Content({ currentCollection, setCurrentCollection, collections, setCollections }) {
   return (
     <div className="Content">
-      <TaskCollections setCollections={setCollections} setCurrentCollection={setCurrentCollection} collections={collections} />
-      {
-        collections.length>0?(<TaskCollection collections={collections} currentCollection={currentCollection} collection={collections[currentCollection]} setCollections={setCollections} />):(<p>Please create a collection</p>)
-      }
+      <TaskCollections currentCollection={currentCollection} 
+        setCollections={setCollections} 
+        setCurrentCollection={setCurrentCollection} 
+        collections={collections} />
+        {
+          collections.length>0?(<TaskCollection collections={collections} 
+            currentCollection={currentCollection} 
+            collection={collections[currentCollection]} 
+            setCollections={setCollections} />):(<p>Create a collection and add new tasks</p>)
+        }
       
     </div>
   );
@@ -212,30 +255,65 @@ function Content({ currentCollection, setCurrentCollection, collections, setColl
 
 
 function App() {
-  
+  const initalCollections = [
+    {
+      title: "Anime Watch List",
+      collection: [
+        {
+          title: "Toaru Majutsu no Index III",
+          checked: true,
+        },
+      ],
+    },
 
-  const [collections, setCollections] = useState([]);
+    {
+      title: "Manga Reading List",
+      collection: [
+        {
+          title: "20th Century Boys",
+          checked: false,
+        },
+        {
+          title: "Jojo's Bizarre Adventure Part 8",
+          checked: false,
+        },
+        {
+          title: "Naruto Shippuden",
+          checked: false,
+        },
+      ],
+    },
+
+    {
+      title: "Technology STack List",
+      collection: [
+        {
+          title: "Learn React",
+          checked: true,
+        },
+        {
+          title: "Learn React Native",
+          checked: true,
+        },
+        {
+          title: "Learn Wordpress",
+          checked: false,
+        },
+      ],
+    },
+  ]
+  const [collections, setCollections] = useState(initalCollections);
   const [currentCollection, setCurrentCollection] = useState(0);
 
-  useEffect(()=>{
-    let collections = [];
-    db.collection("collections").get().then(function(querySnapshot){
-      querySnapshot.forEach(function(doc){
-        console.log(doc.id, " ===> ", doc.data());
-        collections.push(doc.data());
-      });
-      setCollections(collections);
-    });
-  },[]);
-
   return (
-      <div className="App">
-        {
-          collections.length>0?( <Header title={collections[currentCollection].title} />):( <Header title="Aucune Collection Disponible" />)
-        }
-       
-        <Content collections={collections} setCollections={setCollections} currentCollection={currentCollection} setCurrentCollection={setCurrentCollection} />
-      </div>
+    <div className="App">
+      {
+        collections.length>0?(<Header title={collections[currentCollection].title} />)
+        :(<Header title="Please Create a new Collection" />)
+      }
+      
+      <Content collections={collections} setCollections={setCollections} currentCollection={currentCollection} setCurrentCollection={setCurrentCollection} />
+    </div>
   );
 }
 
